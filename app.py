bd7bf7bcd960f96e32299d3524e3bbf8db888648
Hanzal Salim <hanzalsalim94@gmail.com>
2015-04-23 01:36:23 +0530

    Answers any country's capital

--- app.py
+++ app.py
@@ -29,47 +29,59 @@ def index():
 				q_noun.append(i[0])
 
 		for idx,i in enumerate(q_noun):
-			pty = Properties.query.filter(Properties.label.like("%"+i+"%")).first():
+			pty = Properties.query.filter(Properties.label.like("%"+i+"%")).first()
 			if pty:
+				app.logger.info(repr(pty))
 				pid = pty.pid
+				app.logger.info(repr(pid))
 				del q_noun[idx]
-			else
+			else:
 				flash("Property not found",'warning')
 				return render_template('index.html',page="home")
 
-		for i in q_noun
+		for i in q_noun:
 			ur = "https://www.wikidata.org/w/api.php?action=wbsearchentities&search="+i+"&format=json&language=en"
-				response = urllib2.urlopen(ur)
-				data = json.load(response)
-				if data['success'] == 1:
-					qid = data['search'][0]['id']
-				else
-					flash("Item not found",'warning')
-					return render_template('index.html',page="home")
-
-
-		ur = "https://www.wikidata.org/w/api.php?action=wbgetentities&ids="+qid+"&format=json&languages=en"
 			response = urllib2.urlopen(ur)
 			data = json.load(response)
+			app.logger.info(repr(i))
 			if data['success'] == 1:
-				obj = data['entities'][qid]['claims'][pid][0]['mainsnak']['datatype']
-				if obj == "wikibase-item":
-					u = "https://www.wikidata.org/w/api.php?action=wbgetentities&ids="+obj+"&format=json&language=en"
-					response1 = urllib2.urlopen(u)
-					data1 = json.load(response1)
-					data1['entities']['Q668']['claims']['P36'][0]['mainsnak']['datavalue']['value']['numeric-id']
-
-
-
-
-
-
-			else
+				qid = data['search'][0]['id']
+				app.logger.info(repr("Qid  : "+qid))
+			else:
 				flash("Item not found",'warning')
 				return render_template('index.html',page="home")
 
 
-        
+		ur = "https://www.wikidata.org/w/api.php?action=wbgetentities&ids="+qid+"&format=json&languages=en"
+		response = urllib2.urlopen(ur)
+		data = json.load(response)
+		if data['success'] == 1:
+			obj = data['entities'][qid]['claims'][pid][0]['mainsnak']['datatype']
+			if obj == "wikibase-item":
+				value_id = data['entities'][qid]['claims'][pid][0]['mainsnak']['datavalue']['value']['numeric-id']
+				app.logger.info(repr(value_id))
+				
+				u = "https://www.wikidata.org/w/api.php?action=wbgetentities&ids="+"Q"+str(value_id)+"&format=json&languages=en"
+				response1 = urllib2.urlopen(u)
+				data2 = json.load(response1)
+				if data2['success'] == 1:
+					value = data2['entities']['Q'+str(value_id)]['labels']['en']['value']
+					flash(value,'success')
+					return render_template('index.html',page="home")
+				else:
+					flash("Item not found",'warning')
+					return render_template('index.html',page="home")
+			else:
+				value = data['entities'][qid]['claims'][pid][0]['mainsnak']['value']
+				flash(value,'success')
+				return render_template('index.html',page="home")
+
+		else:
+			flash("Item not found",'warning')
+			return render_template('index.html',page="home")
+
+
+    
 
 
 
