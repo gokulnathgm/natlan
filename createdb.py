7ac7006b5ef4e807e791bab3783b50e66b45d244
hanzal <hanzalsalim94@gmail.com>
2015-04-24 10:59:55 +0530

    Copied Complete Properties to DB

--- createdb.py
+++ createdb.py
@@ -7,7 +7,7 @@ db.init_app(app)
 with app.app_context():
 	from app import Properties
 
-	response1 = urllib2.urlopen('https://www.wikidata.org/w/api.php?action=wbgetentities&ids=Q668&format=json&languages=en')
+	"""response1 = urllib2.urlopen('https://www.wikidata.org/w/api.php?action=wbgetentities&ids=Q668&format=json&languages=en')
 	data = json.load(response1)   
 
 	for entity in data['entities'].keys():
@@ -15,15 +15,22 @@ with app.app_context():
 			ur = "https://www.wikidata.org/w/api.php?action=wbgetentities&ids="+pid+"&format=json&languages=en"
 			print ur
 			response2 = urllib2.urlopen(ur)
-			data2 = json.load(response2)
-			for ent in data2['entities']:
-				label = data2['entities'][ent]['labels']['en']['value']
-				print label
-				p = Properties(label, pid)
-				db.session.add(p)
-				db.session.commit()
+
+		data2 = json.load(response2)
+		for ent in data2['entities']:
+			label = data2['entities'][ent]['labels']['en']['value']
+			print label
+			p = Properties(label, pid)
+			db.session.add(p)
+			db.session.commit()"""
 
     
 
-
+	with open('properties-en.json') as data_file:
+		data = json.load(data_file)
+		for ent in data['properties']:
+			label = data['properties'][ent]
+			p = Properties(label, ent)
+			db.session.add(p)
+			db.session.commit()
 
