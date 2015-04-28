af5f1b5450ce8a1297682cb078bb2f9c7432e05a
Gokul <gokulnathgm@gmail.com>
2015-04-28 12:39:43 +0530

    Changes made:  - Answers "What/who is something/someone" questions  - Time format is displayed

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, render_template, url_for, redirect, flash, session, g, send_file, abort
 from sqlalchemy.exc import IntegrityError
-import json,nltk,urllib2,re,pattern.en
+import json,nltk,urllib2,re,pattern.en,calendar
 
 
 app = Flask(__name__)
@@ -16,6 +16,7 @@ def index():
 	if request.method == 'GET':
 	    return render_template('index.html',page="home")
 	elif request.method == 'POST':
+		des = False
 		app.logger.info(repr(request.form))
 		question = request.form['question']
 		q_tagged = pattern.en.tag(question)				#tags the question
@@ -53,6 +54,7 @@ def index():
 		pty =False
 
 		if len(q_noun) == 1:
+			des = True
 			pty=Properties.query.filter(Properties.pid == "P31").all()
 
 		else:
@@ -119,58 +121,76 @@ def index():
 		response = urllib2.urlopen(ur)
 		data = json.load(response)
 
-		if 'claims' in data['entities'][qid]:			#checks whether entity has any statements
-			
-			for prop in pty:
-				pid = prop.pid
-				
-				if pid in data['entities'][qid]['claims']:	#checks whether entity has the given property
-					app.logger.info(repr("pid = " + str(pid)))
-					obj = data['entities'][qid]['claims'][pid][0]['mainsnak']['datatype']
-					app.logger.info(repr(obj))
-					if obj == "wikibase-item":				#property value is another entity
-						value=""
-						for i in range(len(data['entities'][qid]['claims'][pid])):				#gets value from property page
-
-							value_id = data['entities'][qid]['claims'][pid][i]['mainsnak']['datavalue']['value']['numeric-id']
-							app.logger.info(repr(value_id))
-						
-							u = "https://www.wikidata.org/w/api.php?action=wbgetentities&ids="+"Q"+str(value_id)+"&format=json&languages=en"
-							response1 = urllib2.urlopen(u)
-							data2 = json.load(response1)
-							if data2['success']:
-								value = value+"  "+data2['entities']['Q'+str(value_id)]['labels']['en']['value']
-							else:
-								flash("Value can't be found..",'warning')
-								return render_template('index.html',page="home")
-
-						flash(value,'success')
-						return render_template('index.html',page="home")
-
-					elif obj == "string" or obj == "url":			#if property value is string or url
-						value = data['entities'][qid]['claims'][pid][0]['mainsnak']['datavalue']['value']
-						flash(value,'success')
-						return render_template('index.html',page="home")
-					
-					elif obj == "globe-coordinate":					#if property value is geo coordinates
-						latvalue = data['entities'][qid]['claims'][pid][0]['mainsnak']['datavalue']['value']['latitude']
-						lonvalue = data['entities'][qid]['claims'][pid][0]['mainsnak']['datavalue']['value']['longitude']
-						value = "latitude: {} longitude: {} ".format(latvalue,lonvalue)
-						flash(value,'success')
-						return render_template('index.html',page="home")
-
-					else:											#for all other property values
-						value = data['entities'][qid]['claims'][pid][0]['mainsnak']['datavalue']['value']['amount']
-						flash(value,'success')
-						return render_template('index.html',page="home")
-
-			
-			flash("Property not found",'warning')
+		if des == True:
+			value = data['entities'][qid]['descriptions']['en']['value']
+			flash(value,'success')
 			return render_template('index.html',page="home")
 
 		else:
-			flash("Item not found",'warning')
-			return render_template('index.html',page="home")
+
+			if 'claims' in data['entities'][qid]:			#checks whether entity has any statements
+				
+				for prop in pty:
+					pid = prop.pid
+					app.logger.info(repr("Pid  : "+pid))
+					
+					if pid in data['entities'][qid]['claims']:	#checks whether entity has the given property
+						app.logger.info(repr("pid = " + str(pid)))
+						obj = data['entities'][qid]['claims'][pid][0]['mainsnak']['datatype']
+						app.logger.info(repr(obj))
+						if obj == "wikibase-item":				#property value is another entity
+							value=""
+							for i in range(len(data['entities'][qid]['claims'][pid])):				#gets value from property page
+
+								value_id = data['entities'][qid]['claims'][pid][i]['mainsnak']['datavalue']['value']['numeric-id']
+								app.logger.info(repr(value_id))
+							
+								u = "https://www.wikidata.org/w/api.php?action=wbgetentities&ids="+"Q"+str(value_id)+"&format=json&languages=en"
+								response1 = urllib2.urlopen(u)
+								data2 = json.load(response1)
+								if data2['success']:
+									value = value+"  "+data2['entities']['Q'+str(value_id)]['labels']['en']['value']
+								else:
+									flash("Value can't be found..",'warning')
+									return render_template('index.html',page="home")
+
+							flash(value,'success')
+							return render_template('index.html',page="home")
+
+						elif obj == "string" or obj == "url":			#if property value is string or url
+							value = data['entities'][qid]['claims'][pid][0]['mainsnak']['datavalue']['value']
+							flash(value,'success')
+							return render_template('index.html',page="home")
+						
+						elif obj == "globe-coordinate":					#if property value is geo coordinates
+							latvalue = data['entities'][qid]['claims'][pid][0]['mainsnak']['datavalue']['value']['latitude']
+							lonvalue = data['entities'][qid]['claims'][pid][0]['mainsnak']['datavalue']['value']['longitude']
+							value = "latitude: {} longitude: {} ".format(latvalue,lonvalue)
+							flash(value,'success')
+							return render_template('index.html',page="home")
+
+						elif obj == "time":
+							time = data['entities'][qid]['claims'][pid][0]['mainsnak']['datavalue']['value']['time']
+							time = time.split('-')
+							year = time[0][8:12]
+							day = time[2][:2]
+							month = calendar.month_name[time[1]]
+							value = "{}th {} {}".format(day,month,year)
+							flash(value,'success')
+							return render_template('index.html',page="home")
+
+						else:											#for all other property values
+							value = data['entities'][qid]['claims'][pid][0]['mainsnak']['datavalue']['value']['amount']
+							flash(value,'success')
+							return render_template('index.html',page="home")
+
+				
+				flash("Property not found",'warning')
+				return render_template('index.html',page="home")
+
+			else:
+				flash("Item not found",'warning')
+				return render_template('index.html',page="home")
 
 
     
